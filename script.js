0742860a57fa112623adc15f502a318c447db9db
garima <garimadhall15@gmail.com>
2020-06-07 19:16:48 +0530

    updated code

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ var xlxs=require("xlsx");
 var wb=xlxs.readFile("Dataset.xlsx",{cellDates:true});
 var ws=wb.Sheets["school dataset"];
 var data=xlxs.utils.sheet_to_json(ws);
-xlxs.writeFile(data,"Dataset.json");
+
 
 
