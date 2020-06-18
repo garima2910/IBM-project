71df17f2aa67201c3f85e48467f9402bb442ceba
garima <garimadhall15@gmail.com>
2020-06-18 22:31:26 +0530

    updated code in script.js

--- script.js
+++ script.js
@@ -1,10 +1,44 @@
 
+//using MongoDB module
+var MongoClient = require('mongodb').MongoClient
 let csvToJson = require('convert-csv-to-json');
- 
-let fileInputName = 'student_data2.csv'; 
-let fileOutputName = 'output.json';
- 
-csvToJson.generateJsonFileFromCsv(fileInputName,fileOutputName);
+function makejsonfile(fileInputName){
+    let fileOutputName = 'output.json';
+    csvToJson.generateJsonFileFromCsv(fileInputName,fileOutputName);
+}
+makejsonfile('student_data2.csv');
+var url = "mongodb://localhost:27017/studentDB";
+MongoClient.connect(url, function(err, db) {
+	if (err) throw err;
+	console.log("Database created!");
+	db.close();
+});
+MongoClient.connect(url, function(err, db) {
+	if (err) throw err;
+	// specifying the DB name
+	var dbo = db.db("studentDB");
+	// creating collection
+	dbo.createCollection("StudentRecord", function(err, res) {
+		if (err) throw err;
+		console.log("Collection created!");
+		db.close();
+	});
+}); 
+MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    // specifying the DB name
+    var dbo = db.db("studentDB");
+    // reading the JSON file
+    var myobj = require("./output.json")
+  
+    // inserting the JSON file into 'StudentRecord' collection in 'studentDB' database
+    dbo.collection("StudentRecord").insertMany(myobj, function(err, res) {
+      if (err) throw err;
+      console.log("Number of documents inserted: " + res.insertedCount);
+      db.close();
+    });
+  });
+
 
 
 
